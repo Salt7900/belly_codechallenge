ccd4209df25320c41d17a68f65de89e07cce71e9
Salt7900 <bnfallon@gmail.com>
2015-12-15 16:01:09 -0600

    add location class

---
+++ belly_cc/Location.swift
@@ -0,0 +1,13 @@
+//
+//  Location.swift
+//  belly_cc
+//
+//  Created by Ben Fallon on 12/15/15.
+//  Copyright © 2015 Ben Fallon. All rights reserved.
+//
+
+import Foundation
+
+class Location {
+    <#properties and methods#>
+}
