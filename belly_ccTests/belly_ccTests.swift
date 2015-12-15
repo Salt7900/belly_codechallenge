88ab942a2d036f5e1b42146eac8ef5bf4b066757
Salt7900 <bnfallon@gmail.com>
2015-12-15 15:25:20 -0600

    Initial Commit

---
+++ belly_ccTests/belly_ccTests.swift
@@ -0,0 +1,36 @@
+//
+//  belly_ccTests.swift
+//  belly_ccTests
+//
+//  Created by Ben Fallon on 12/15/15.
+//  Copyright © 2015 Ben Fallon. All rights reserved.
+//
+
+import XCTest
+@testable import belly_cc
+
+class belly_ccTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
