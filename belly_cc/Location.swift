db9796ed83d77fa54eff7ab66be828f43092e17d
Salt7900 <bnfallon@gmail.com>
2015-12-15 16:06:24 -0600

    add location class

--- belly_cc/Location.swift
+++ belly_cc/Location.swift
@@ -9,5 +9,17 @@
 import Foundation
 
 class Location {
-    <#properties and methods#>
+    var name:String
+    var lat:Double
+    var long:Double
+    var category:String
+    var imageUrl:String
+
+    init(name: String, lat: Double, long:Double, category:String, imageUrl: String){
+        self.name = name;
+        self.lat = lat;
+        self.long = long;
+        self.category = category;
+        self.imageUrl = imageUrl
+    }
 }
