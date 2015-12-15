28fe3404dbc4eaad7f947b1260bd75cc5c7765dd
Salt7900 <bnfallon@gmail.com>
2015-12-15 16:14:58 -0600

    add dummy locations

--- belly_cc/ViewController.swift
+++ belly_cc/ViewController.swift
@@ -11,10 +11,12 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet var locationTableView: UIView!
+    var arrayOfLocations = [Location]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        setUpLocations()
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,6 +24,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func setUpLocations(){
+        var location1 = Location(name: "Bill's Steakhouse", lat: 41.810446, long: -87.892233, category: "Food/Dining", imageUrl: "http://www.joomlaworks.net/images/demos/galleries/abstract/7.jpgs")
+        var location2 = Location(name: "Starbucks", lat: 41.808430, long: -87.902253, category: "Coffee", imageUrl: "https://upload.wikimedia.org/wikipedia/en/thumb/3/35/Starbucks_Coffee_Logo.svg/1024px-Starbucks_Coffee_Logo.svg.png")
+        
+        arrayOfLocations.append(location1)
+        arrayOfLocations.append(location2)
+    }
 
 }
 
