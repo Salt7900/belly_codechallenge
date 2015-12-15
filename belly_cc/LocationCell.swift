0959208a4fb38b5e00c15fc872a78f6254d67705
Salt7900 <bnfallon@gmail.com>
2015-12-15 15:48:36 -0600

    add custom cell class

---
+++ belly_cc/LocationCell.swift
@@ -0,0 +1,24 @@
+//
+//  LocationCell.swift
+//  belly_cc
+//
+//  Created by Ben Fallon on 12/15/15.
+//  Copyright © 2015 Ben Fallon. All rights reserved.
+//
+
+import UIKit
+
+class LocationCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
