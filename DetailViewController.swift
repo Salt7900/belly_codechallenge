7c7c4c5d9312a813bb315d421959656d3a6ee7ff
Salt7900 <bnfallon@gmail.com>
2015-12-17 14:55:14 -0600

    update layout of detail page

--- DetailViewController.swift
+++ DetailViewController.swift
@@ -32,12 +32,10 @@ class DetailViewController: UIViewController {
         
         setupMap()
     
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
