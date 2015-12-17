d08402e6e7f84548907f31ae3bb8b3b04c8830e1
Salt7900 <bnfallon@gmail.com>
2015-12-17 16:04:14 -0600

    add reachabilty helper

---
+++ belly_cc/Reachability.swift
@@ -0,0 +1,31 @@
+//
+//  Reachability.swift
+//  belly_cc
+//
+//  Created by Ben Fallon on 12/17/15.
+//  Copyright © 2015 Ben Fallon. All rights reserved.
+//
+
+import SystemConfiguration
+
+func connectedToNetwork() -> Bool {
+    
+    var zeroAddress = sockaddr_in()
+    zeroAddress.sin_len = UInt8(sizeofValue(zeroAddress))
+    zeroAddress.sin_family = sa_family_t(AF_INET)
+    
+    guard let defaultRouteReachability = withUnsafePointer(&zeroAddress, {
+        SCNetworkReachabilityCreateWithAddress(nil, UnsafePointer($0))
+    }) else {
+        return false
+    }
+    
+    var flags : SCNetworkReachabilityFlags = []
+    if !SCNetworkReachabilityGetFlags(defaultRouteReachability, &flags) {
+        return false
+    }
+    
+    let isReachable = flags.contains(.Reachable)
+    let needsConnection = flags.contains(.ConnectionRequired)
+    return (isReachable && !needsConnection)
+}
