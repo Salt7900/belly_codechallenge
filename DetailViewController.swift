27de3150b4a20f9037d6a1635aa9e04dc39d7a05
Salt7900 <bnfallon@gmail.com>
2015-12-17 15:37:32 -0600

    remove didReciveMemoryWarning

--- DetailViewController.swift
+++ DetailViewController.swift
@@ -34,9 +34,6 @@ class DetailViewController: UIViewController {
     
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-    }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
         self.dismissViewControllerAnimated(true, completion: nil)
